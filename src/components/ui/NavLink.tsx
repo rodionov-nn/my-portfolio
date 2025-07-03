d69f97496c28b0a0a816d9d2d15e4767661a27c1
unknown <rodionov.nmx@gmail.com>
2025-07-03 22:46:14 +0200

    NavLink color fix

--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -15,7 +15,7 @@ export default function NavLink({
   className = "",
 }: NavLinkProps) {
   const baseClass =
-    "relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-[#111d13] dark:after:bg-[#cfd4c5] after:transition-all hover:after:w-full";
+    "relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-neutral-800 dark:after:bg-neutral-200 after:transition-all hover:after:w-full";
 
   return (
     <Link href={href} className={cn(baseClass, className)}>
